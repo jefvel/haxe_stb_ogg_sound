851fd29eec79eea2e8fabd9b26557ef620096d88
Aksel <jefvel@users.noreply.github.com>
2025-04-10 18:57:49 +0200

    Update Reader.hx
    cast to correct type

--- src/stb/format/vorbis/Reader.hx
+++ src/stb/format/vorbis/Reader.hx
@@ -76,7 +76,7 @@ class Reader {
     function new (input:Input, seekFunc:Int->Void, inputLength:Int) {
         this.seekFunc = seekFunc;
         this.inputLength = inputLength;
-        decoder = VorbisDecoder.start(input);
+        decoder = cast(VorbisDecoder.start(input), VorbisDecoder);
         decoder.setupSampleNumber(seekFunc, inputLength);
         loopStart = header.comment.loopStart;
         loopLength = header.comment.loopLength;
